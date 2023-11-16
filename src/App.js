4fb6d65a985d730b300004134fae7e5bea1c7e99
Lucas Rocha <l.rochasantos20@gmail.com>
2023-11-16 11:42:05 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+
+import React, { useEffect, useState } from 'react';
 import './App.css';
+  import { createClient } from 'pexels';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function App() {
+    const [query, setQuery] = useState('');
+    const [photos, setPhotos] = useState([]);
+    const [videosa, setVideo] = useState([]);
+    
+    const client = createClient('T3hNun21w2MhkeflmOh72keMzwriuc2QARDDafnuoRFwVjXGnccQl6OE');
+  
+    useEffect(() => {
+      const fetchPhotosAndVideos  = async () => {
+        try {
+          const photoResponse = await client.photos.curated({ per_page: 16});
+          setPhotos(photoResponse.photos);
 
-export default App;
+          const videoResponse = await client.videos.popular({ per_page: 80 });
+          const filteredVideos = videoResponse.videos.filter(video => video.height >= 2732);
+          setVideo(filteredVideos);
+        } catch (error) {
+          console.error('Erro ao buscar fotos:', error);
+        }
+      };
+  
+     
+      fetchPhotosAndVideos ();
+    }, []);
+
+    const handleSearch = async () => {
+      try {
+        // Buscar fotos
+        const photoResponse = await client.photos.search({ query, per_page: 16 });
+        setPhotos(photoResponse.photos);
+    
+        // Buscar vídeos
+        const videoResponse = await client.videos.search({ query, per_page: 80 });
+        const filteredVideos = videoResponse.videos.filter(video => video.height >= 2732);
+        setVideo(filteredVideos);
+      } catch (error) {
+        console.error('Erro ao buscar fotos ou vídeos:', error);
+      }
+    };
+
+      // Combine as listas de fotos e vídeos em uma única lista
+  const combinedList = [...photos, ...videosa];
+
+  // Embaralhe a lista combinada
+  const shuffledList = combinedList.sort(() => Math.random() - 0.5);
+
+    return (
+      <div className="App">
+         <div id='geralbtn'>
+         <label htmlFor="fileInput">
+        <div id="fileinp">+</div>
+      </label>
+      <input
+        type="file"
+        id="fileInput"
+        style={{ display: 'none' }}
+      />
+          
+        <input
+          type='text'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className='button-30' onClick={handleSearch}>Buscar</button>
+      </div>
+      <section className="App-header">
+      {shuffledList.map((item) => (
+        <div className='sectionphoto' key={item.id}>
+          {item.hasOwnProperty('src') ? (
+            <img src={item.src.original} alt={`Photo by ${item.photographer}`} />
+          ) : (
+            <video loop autoPlay muted>
+              <source src={item.video_files[0].link} />
+            </video>
+          )}
+        </div>
+      ))}
+    </section>
+      </div>
+    );
+  }
+  
+  export default App;
