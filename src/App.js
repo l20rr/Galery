fff3d7ee5499cba3b92171c54849e380540b9772
Lucas Rocha <l.rochasantos20@gmail.com>
2023-11-17 10:42:34 +0000

    select photo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import './App.css';
   import { createClient } from 'pexels';
@@ -7,7 +6,8 @@ import './App.css';
     const [query, setQuery] = useState('');
     const [photos, setPhotos] = useState([]);
     const [videosa, setVideo] = useState([]);
-    
+    const [big, setBig] = useState(false);
+    const [selectedPhoto, setSelectedPhoto] = useState(null);
     const client = createClient('T3hNun21w2MhkeflmOh72keMzwriuc2QARDDafnuoRFwVjXGnccQl6OE');
   
     useEffect(() => {
@@ -19,21 +19,21 @@ import './App.css';
           const videoResponse = await client.videos.popular({ per_page: 80 });
           const filteredVideos = videoResponse.videos.filter(video => video.height >= 2732);
           setVideo(filteredVideos);
+
         } catch (error) {
           console.error('Erro ao buscar fotos:', error);
         }
       };
-  
-     
       fetchPhotosAndVideos ();
     }, []);
-
+    
+   
     const handleSearch = async () => {
       try {
         // Buscar fotos
         const photoResponse = await client.photos.search({ query, per_page: 16 });
         setPhotos(photoResponse.photos);
-    
+        setBig(false);
         // Buscar vídeos
         const videoResponse = await client.videos.search({ query, per_page: 80 });
         const filteredVideos = videoResponse.videos.filter(video => video.height >= 2732);
@@ -43,44 +43,68 @@ import './App.css';
       }
     };
 
-      // Combine as listas de fotos e vídeos em uma única lista
+ 
+    const BigImg = async (ID) => {
+      try {
+        const photoResponse = await client.photos.show({ id: ID });
+        // Armazena os detalhes da foto para renderização individual
+        console.log(photoResponse)
+        setSelectedPhoto(photoResponse);
+        setBig(true);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  // Combine as listas de fotos e vídeos em uma única lista
   const combinedList = [...photos, ...videosa];
-
   // Embaralhe a lista combinada
   const shuffledList = combinedList.sort(() => Math.random() - 0.5);
 
     return (
       <div className="App">
-         <div id='geralbtn'>
-         <label htmlFor="fileInput">
-        <div id="fileinp">+</div>
-      </label>
-      <input
-        type="file"
-        id="fileInput"
-        style={{ display: 'none' }}
-      />
-          
-        <input
+       <div id='geralbtn'>
+        <button className='button-30'>
+          Home
+        </button>
+     
+       <input
           type='text'
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
         <button className='button-30' onClick={handleSearch}>Buscar</button>
+     
       </div>
       <section className="App-header">
-      {shuffledList.map((item) => (
-        <div className='sectionphoto' key={item.id}>
+    {big ? (
+      <div className='enlarged-photo'>
+        <div className='sectionphoto2'>
+         <img  src={selectedPhoto.src.original} alt={`Photo by ${selectedPhoto.photographer}`} />
+        </div>
+        <div className='btnsbig'>
+        <button onClick={() => setBig(false)}>X</button>
+        <div id='btngeral'>
+        <button className='btndisplay' onClick={() => setBig(false)}>Mobile</button>
+        <button className='btndisplay' onClick={() => setBig(false)}>Descktop</button>
+        <button className='btndisplay' onClick={() => setBig(false)}>Tablet</button>
+        
+        </div>
+        </div>
+      </div>
+    ) : (
+      shuffledList.map((item) => (
+        <div className='sectionphoto' key={item.id} onClick={() => BigImg(item.id)}>
           {item.hasOwnProperty('src') ? (
-            <img src={item.src.original} alt={`Photo by ${item.photographer}`} />
+            <img  src={item.src.portrait} alt={`Photo by ${item.photographer}`} />
           ) : (
             <video loop autoPlay muted>
               <source src={item.video_files[0].link} />
             </video>
           )}
         </div>
-      ))}
-    </section>
+      ))
+    )}
+  </section>
       </div>
     );
   }
