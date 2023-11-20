c56bb1ed4b42f8eb301dc9ace38be0fcdc2d201e
Lucas Rocha <l.rochasantos20@gmail.com>
2023-11-20 19:07:12 +0000

    app com img 100%

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
   import { createClient } from 'pexels';
+  import SmartphoneIcon from '@mui/icons-material/Smartphone';
+  import DesktopMacIcon from '@mui/icons-material/DesktopMac';
+  import TabletIcon from '@mui/icons-material/Tablet';
 
   function App() {
     const [query, setQuery] = useState('');
@@ -8,12 +11,13 @@ import './App.css';
     const [videosa, setVideo] = useState([]);
     const [big, setBig] = useState(false);
     const [selectedPhoto, setSelectedPhoto] = useState(null);
+    const [size, setSize] = useState('mobile');
     const client = createClient('T3hNun21w2MhkeflmOh72keMzwriuc2QARDDafnuoRFwVjXGnccQl6OE');
   
     useEffect(() => {
       const fetchPhotosAndVideos  = async () => {
         try {
-          const photoResponse = await client.photos.curated({ per_page: 16});
+          const photoResponse = await client.photos.curated({ per_page: 50});
           setPhotos(photoResponse.photos);
 
           const videoResponse = await client.videos.popular({ per_page: 80 });
@@ -31,7 +35,7 @@ import './App.css';
     const handleSearch = async () => {
       try {
         // Buscar fotos
-        const photoResponse = await client.photos.search({ query, per_page: 16 });
+        const photoResponse = await client.photos.search({ query, per_page: 50 });
         setPhotos(photoResponse.photos);
         setBig(false);
         // Buscar vídeos
@@ -43,6 +47,26 @@ import './App.css';
       }
     };
 
+    const handleButtonClick = (newSize) => {
+      setSize(newSize);
+    };
+  
+    const handleDownloadButtonClick = () => {
+      if (selectedPhoto) {
+        fetch(selectedPhoto.src.original)
+          .then(response => response.blob())
+          .then(blob => {
+            const url = window.URL.createObjectURL(new Blob([blob]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', `photo_by_${selectedPhoto.photographer}.jpg`);
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+          })
+          .catch(error => console.error('Error downloading image:', error));
+      }
+    };
  
     const BigImg = async (ID) => {
       try {
@@ -63,10 +87,6 @@ import './App.css';
     return (
       <div className="App">
        <div id='geralbtn'>
-        <button className='button-30'>
-          Home
-        </button>
-     
        <input
           type='text'
           value={query}
@@ -78,16 +98,20 @@ import './App.css';
       <section className="App-header">
     {big ? (
       <div className='enlarged-photo'>
-        <div className='sectionphoto2'>
+       <div id='space'>
+       <div className={`sectionphoto2 ${size}`}>
          <img  src={selectedPhoto.src.original} alt={`Photo by ${selectedPhoto.photographer}`} />
         </div>
+       </div>
         <div className='btnsbig'>
-        <button onClick={() => setBig(false)}>X</button>
+        <button id='x' onClick={() => setBig(false)}>X</button>
         <div id='btngeral'>
-        <button className='btndisplay' onClick={() => setBig(false)}>Mobile</button>
-        <button className='btndisplay' onClick={() => setBig(false)}>Descktop</button>
-        <button className='btndisplay' onClick={() => setBig(false)}>Tablet</button>
-        
+          <h2>Choose size <br/> download image</h2>
+         
+        <button className='btndisplay' onClick={() => handleButtonClick('mobile')}><SmartphoneIcon/></button>
+          <button className='btndisplay' onClick={() => handleButtonClick('desktop')}><DesktopMacIcon/></button>
+          <button className='btndisplay' onClick={() => handleButtonClick('tablet')}><TabletIcon/></button>
+          <button className='button-30 btn3'  onClick={handleDownloadButtonClick} >Download</button>
         </div>
         </div>
       </div>
